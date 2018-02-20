186333f64dc6926a1af609b9a35853638728704d
LPNOIDA01 <akhilesh.singh@lateralpraxis.com>
2018-02-20 11:30:47 +0530

    Push

---
+++ app/src/main/java/lateralpraxis/lpdnd/primaryreceipt/primary_receipt_create.java
@@ -0,0 +1,15 @@
+package lateralpraxis.lpdnd.primaryreceipt;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import lateralpraxis.lpdnd.R;
+
+public class primary_receipt_create extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_primary_receipt_create);
+    }
+}
---
+++ app/src/main/java/lateralpraxis/lpdnd/primaryreceipt/view_primary_receipt.java
@@ -0,0 +1,15 @@
+package lateralpraxis.lpdnd.primaryreceipt;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import lateralpraxis.lpdnd.R;
+
+public class view_primary_receipt extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_primary_receipt);
+    }
+}
