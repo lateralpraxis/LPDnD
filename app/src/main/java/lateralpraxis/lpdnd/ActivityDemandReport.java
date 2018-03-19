e106eb7d45b14034742efb9bfe08ba1e33dc3ecc
pranav.pradakshine <pranav.pradakshine@lateralpraxis.com>
2018-03-19 18:01:43 +0530

    Added Company Dropdown

--- app/src/main/java/lateralpraxis/lpdnd/ActivityDemandReport.java
+++ app/src/main/java/lateralpraxis/lpdnd/ActivityDemandReport.java
@@ -67,7 +67,7 @@ public class ActivityDemandReport  extends Activity {
 	private TextView tvDate,tvEmpty;
 	private ListView listDemand;
 	private Button btnGo;
-	private Spinner spCentre;
+	private Spinner spCentre, spCompany;
 	/*End of code to declare Controls*/
 	//On create method similar to page load
 	@Override
@@ -101,6 +101,7 @@ public class ActivityDemandReport  extends Activity {
 		tvEmpty = (TextView) findViewById(R.id.tvEmpty);
 		btnGo= (Button) findViewById(R.id.btnGo);
 		spCentre= (Spinner) findViewById(R.id.spCentre);
+		spCompany= (Spinner) findViewById(R.id.spCompany);
 		dateFormatter_display = new SimpleDateFormat("dd-MMM-yyyy", Locale.US);
 		dateFormatter_database = new SimpleDateFormat("yyyy-MM-dd", Locale.US);
 
@@ -113,6 +114,9 @@ public class ActivityDemandReport  extends Activity {
 		if (common.isConnected()) {
 			AsyncCentreWSCall task = new AsyncCentreWSCall();
 			task.execute();
+
+			AsyncCompanyWSCall taskComp = new AsyncCompanyWSCall();
+			taskComp.execute();
 		}
 
 		btnGo.setOnClickListener(new View.OnClickListener() {
@@ -121,6 +125,8 @@ public class ActivityDemandReport  extends Activity {
 			public void onClick(View arg0) {
 				if(spCentre.getSelectedItemPosition()==0)
 					common.showToast(lang.equalsIgnoreCase("hi")?"कृपया केंद्र का चयन करें।":"Please select centre.");
+				else if(spCompany.getSelectedItemPosition()==0)
+					common.showToast(lang.equalsIgnoreCase("hi")?"कृपया कंपनी का चयन करें।":"Please select company.");
 				else
 				{
 					if (common.isConnected()) {
@@ -454,6 +460,70 @@ public class ActivityDemandReport  extends Activity {
 		MenuInflater inflater = getMenuInflater();
 		inflater.inflate(R.menu.menu_home, menu);
 		return true;
+	}
+
+	//To bind Company to spCompany
+	private class AsyncCompanyWSCall extends AsyncTask<String, Void, String> {
+		private ProgressDialog Dialog = new ProgressDialog(ActivityDemandReport.this);
+
+		@Override
+		protected String doInBackground(String... params) {
+			try {
+				db.open();
+				db.deleteMISCompany();
+				db.close();
+				String[] name = { "action", "userId", "role" };
+				String[] value = { "ReadCompany", userId, "" };
+				responseJSON = "";
+				// Call method of web service to download route from server
+				responseJSON = common.CallJsonWS(name, value, "ReadMaster",
+						common.url);
+
+			} catch (SocketTimeoutException e) {
+				return "ERROR: TimeOut Exception. Either Server is busy or Internet is slow";
+			} catch (final Exception e) {
+				// TODO: handle exception
+				e.printStackTrace();
+				return "ERROR: " + "Unable to fetch response from server.";
+			}
+			return "";
+		}
+
+		@Override
+		protected void onPostExecute(String result) {
+			try {
+				if (!result.contains("ERROR: ")) {
+					JSONArray jsonArray = new JSONArray(responseJSON);
+
+					for (int i = 0; i < jsonArray.length(); ++i) {
+						db.open();
+						db.Insert_MISCompany(jsonArray.getJSONObject(i).getString("A"), jsonArray.getJSONObject(i).getString("B"));
+						db.close();
+					}
+
+					//Code to bind Centres in spinner
+					spCompany.setAdapter(DataAdapter("miscompany",""));
+				} else {
+					common.showAlert(ActivityDemandReport.this, result, false);
+				}
+			} catch (Exception e) {
+				common.showAlert(ActivityDemandReport.this, "Company Downloading failed: " + e.toString(), false);
+			}
+			Dialog.dismiss();
+		}
+
+		@Override
+		protected void onPreExecute() {
+			Dialog.setMessage("Downloading Company..");
+			Dialog.setCancelable(false);
+			Dialog.show();
+
+		}
+
+		@Override
+		protected void onProgressUpdate(Void... values) {
+		}
+
 	}
 
 
