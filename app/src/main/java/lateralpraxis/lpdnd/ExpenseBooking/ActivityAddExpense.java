9c70cdba4c128070b668635d9a1a0e52d70f926b
LPNOIDA01 <akhilesh.singh@lateralpraxis.com>
2018-04-13 17:48:14 +0530

    Push

--- app/src/main/java/lateralpraxis/lpdnd/ExpenseBooking/ActivityAddExpense.java
+++ app/src/main/java/lateralpraxis/lpdnd/ExpenseBooking/ActivityAddExpense.java
@@ -90,30 +90,6 @@ public class ActivityAddExpense extends Activity {
     private String[] FileNameStrings;
     //</editor-fold>
 
-    //<editor-fold desc="Method to calculate sample size for image">
-    public static int calculateInSampleSize(
-            BitmapFactory.Options options, int reqWidth, int reqHeight) {
-        // Raw height and width of image
-        final int height = options.outHeight;
-        final int width = options.outWidth;
-        int inSampleSize = 1;
-
-        if (height > reqHeight || width > reqWidth) {
-
-            final int halfHeight = height / 2;
-            final int halfWidth = width / 2;
-
-            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
-            // height and width larger than the requested height and width.
-            while ((halfHeight / inSampleSize) > reqHeight
-                    && (halfWidth / inSampleSize) > reqWidth) {
-                inSampleSize *= 2;
-            }
-        }
-        return inSampleSize;
-    }
-    //</editor-fold>
-
     //<editor-fold desc="Method to generate random number and return the same">
     public static String random() {
         Random r = new Random();
